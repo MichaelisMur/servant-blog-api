659b73bee889524cd47f58fd9edc6143c92bbf46
Geen Genade <michaelis.mur@gmail.com>
2022-12-26 22:35:49 +0300

    routing upd

--- app/Main.hs
+++ app/Main.hs
@@ -30,12 +30,12 @@ import Network.URI.Encode
 
 type OurBlogAPI =  "hello"  :> Get '[JSON] String
               :<|> "add"    :> ReqBody '[JSON] PostData :> Post '[JSON] String
-              :<|> "add2"    :> Capture "title" String :> Capture "img" String :> Capture "text" String :> Capture "descr" String :> Post '[JSON] String
               :<|> "delete" :> ReqBody '[JSON] DeleteData :> Post '[JSON] String
-              :<|> "delete2" :> Capture "id" Int :> Post '[JSON] String
               :<|> "post"   :> Capture "x" Int :> Post '[JSON] String
               :<|> "all"    :> Post '[JSON] String
-              :<|> "print"  :> Get '[JSON] String
+              :<|> "add2"    :> Capture "title" String :> Capture "img" String :> Capture "text" String :> Capture "descr" String :> Post '[JSON] String
+              :<|> "delete2" :> Capture "id" Int :> Post '[JSON] String
+
 
 
 data PostData = PostData
@@ -65,15 +65,6 @@ addNewPost post = do
 addPost :: String -> String -> String -> String -> Int -> Action IO [Database.MongoDB.Value]
 addPost text img title descr pi = insertMany "post" [
    ["text" =: text, "img" =: img, "title" =: title, "descr" =: descr, "postId" =: pi]]
-   
-addNewPost2 :: String -> String -> String -> String -> Handler String
-addNewPost2 title img text descr = do
-          pipe <- liftIO $ connect (Database.MongoDB.host "localhost")
-          g <- newStdGen
-          let a :: Int = fst $ randomR (10000000, 99999999) g
-          e <- liftIO $ access pipe master "servantAPI" (addPost text (Network.URI.Encode.decode img) title descr a)
-          liftIO $ close pipe
-          return (show a)
 
 
 deletePost :: DeleteData -> Handler String
@@ -84,14 +75,6 @@ deletePost post = do
           return (show $ postDel post)
 delPost :: Int -> Action IO ()
 delPost pi = Database.MongoDB.deleteOne (select ["postId" =: pi] "post")
-
-
-deletePost2 :: Int -> Handler String
-deletePost2 postId = do
-          pipe <- liftIO $ connect (Database.MongoDB.host "localhost")
-          e <- liftIO $ access pipe master "servantAPI" (delPost postId)
-          liftIO $ close pipe
-          return (show $ postId)
 
 
 getPostById :: Int -> Handler String
@@ -112,10 +95,23 @@ getAllPosts = do
           return (show $ e)
 getAll :: Action IO [Document]
 getAll = rest =<< Database.MongoDB.find (select [] "post") {project = ["_id" =: 0, "text" =: 0]}
+   
 
-
-allTeams :: Action IO [Document]
-allTeams = rest =<< Database.MongoDB.find (select [] "post")
+-- Вторая версия запросов на создание нового поста и удаление (без body, т.к. были проблемы с CORS)
+addNewPost2 :: String -> String -> String -> String -> Handler String
+addNewPost2 title img text descr = do
+          pipe <- liftIO $ connect (Database.MongoDB.host "localhost")
+          g <- newStdGen
+          let a :: Int = fst $ randomR (10000000, 99999999) g
+          e <- liftIO $ access pipe master "servantAPI" (addPost text (Network.URI.Encode.decode img) title descr a)
+          liftIO $ close pipe
+          return (show a)
+deletePost2 :: Int -> Handler String
+deletePost2 postId = do
+          pipe <- liftIO $ connect (Database.MongoDB.host "localhost")
+          e <- liftIO $ access pipe master "servantAPI" (delPost postId)
+          liftIO $ close pipe
+          return (show $ postId)
 
 
 serverRouting :: Server OurBlogAPI
@@ -124,27 +120,15 @@ serverRouting =
           
         :<|> addNewPost
           
-        :<|> addNewPost2
-          
         :<|> deletePost
-          
-        :<|> deletePost2
           
         :<|> getPostById
           
         :<|> getAllPosts
-
-        :<|> do
-            pipe <- liftIO $ connect (Database.MongoDB.host "localhost")
-            e <- liftIO $ access pipe master "servantAPI" allTeams
-            liftIO $ close pipe
-            return $ (show e)
-
-        -- :<|> do
-        --     pipe <- liftIO $ connect (Database.MongoDB.host "localhost")
-        --     e <- liftIO $ access pipe master "servantAPI" (getNextPostId)
-        --     liftIO $ close pipe
-        --     return $ (show $ valueAt "postId" (fromJust e))
+          
+        :<|> addNewPost2
+          
+        :<|> deletePost2
             
             -- manager <- liftIO $ newManager defaultManagerSettings
 
